016e3736c83e34dd06c4ece183da2f12423aa7d5
Ziyan Liu <liuziyan316@gmail.com>
2018-06-02 10:07:11 -0400

    download today's price series

--- query_daily_series.py
+++ query_daily_series.py
@@ -1,27 +1,37 @@
+import requests
+from alpha_api import alpha_api
 from datetime import datetime
+from tickers_to_track import tickers
 time_format = "%Y-%m-%d"
 today_string = datetime.strftime(datetime.today(), time_format)
 
 
 def query_daily_series():
-	for symbol in symbols:
-	    # query data
-	    print('symbol: ', symbol)
-	    daily_url = (
-	    "https://www.alphavantage.co/query?function={query_function}&symbol={stock_code}&outputsize={outputsize}&apikey={apikey}".format(
-	        query_function="TIME_SERIES_DAILY",
-	        stock_code=symbol,
-	        outputsize="full",
-	        apikey=alpha_api))
-	    response = requests.get(url=daily_url).json()
-	    daily_series = {"series": response["Time Series (Daily)"]}
+    for ticker in tickers:
+        # query data
+        print('ticker: ', ticker)
+        daily_url = (
+            "https://www.alphavantage.co/query?function={query_function}&symbol={stock_code}&outputsize={outputsize}&apikey={apikey}".format(
+            query_function="TIME_SERIES_DAILY",
+            stock_code=ticker,
+            outputsize="full",
+            apikey=alpha_api)
+        )
+        response = requests.get(url=daily_url).json()
+        print("daily_url: ", daily_url)
+        try:
+            daily_series = {"series": response["Time Series (Daily)"]}
+        except:
+            print("response: ", response)
 
-	    # store data
-	    import json
-	    output_filename = "daily_prices_series/daily_series_{}_lastDay_{}.json".format(symbol, today_string)
-	    with open(output_filename, 'w') as f:
-	        json.dump(daily_series, f)
-	    f.close()
+        # store data
+        import json
+        output_filename = "daily_prices_series/daily_series_{}_lastDay_{}.json".format(ticker, today_string)
+        with open(output_filename, 'w') as f:
+            json.dump(daily_series, f)
+        f.close()
+
 
 if __name__ == "__main__":
-	query_daily_series()
+    query_daily_series()
+
